9aeb93053f6e1953c26f935cb072d4eb6ba723aa
Kyle Conroy <kyle@kyleconroy.com>
2017-07-31 08:40:26 -0700

    Don't cache on the local filesystem

--- cmd/restcache/main.go
+++ cmd/restcache/main.go
@@ -28,16 +28,16 @@ func main() {
 		log.Fatalf("No bucket provided; please set S3_BUCKET")
 	}
 
-	fs, err := blobstore.NewFileSystem("cas")
-	if err != nil {
-		log.Fatalf("Failed creating FS store: %s", err)
-	}
-
-	size := int64(1000) * 1e+6 // 1GB
-
+	// TODO: Fix RPS limitations with LRU cache
+	// fs, err := blobstore.NewFileSystem("cas")
+	// if err != nil {
+	// 	log.Fatalf("Failed creating FS store: %s", err)
+	// }
+	// size := int64(1000) * 1e+6 // 1GB
+	// cache := blobstore.NewSynchronized(blobstore.LRU(size, fs))
+	// store := blobstore.Prefixed("cas", blobstore.Cached(main, cache))
 	main := blobstore.NewS3(s3.New(sess), bucket)
-	cache := blobstore.NewSynchronized(blobstore.LRU(size, fs))
-	store := blobstore.Prefixed("cas", blobstore.Cached(main, cache))
+	store := blobstore.Prefixed("cas", main)
 
 	log.Printf("Starting CAS server on port %s", port)
 	http.ListenAndServe(":"+port, restcache.NewServer(store))
